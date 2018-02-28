f840f73a736b4b9a5d9fa739d9ebf6f6adb680d3
IvanovIvanSergeevich <ya.ivanov.i.s@gmail.com>
2018-02-28 23:35:23 +0300

    feature/index.js All money exchanged

--- src/index.js
+++ src/index.js
@@ -1,5 +1,50 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+
+    if(currency<=10000) {
+        var cash = [];
+        cash[0] = {
+            name: "H",
+            value: 50,
+            count: 0
+        }
+        cash[1] = {
+            name: "Q",
+            value: 25,
+            count: 0
+        }
+        cash[2] = {
+            name: "D",
+            value: 10,
+            count: 0
+        }
+        cash[3] = {
+            name: "N",
+            value: 5,
+            count: 0
+        }
+        cash[4] = {
+            name: "P",
+            value: 1,
+            count: 0
+        }
+        var i;
+        for (i = 0; i < cash.length; i++) {
+            if (currency >= cash[i].value) {
+                cash[i].count = Math.floor(currency / cash[i].value);
+                currency %= cash[i].value;
+            }
+            else cash[i].count = undefined;
+
+        }
+        var name, result = {};
+        for (i = 0; i < cash.length; i++) {
+            if (cash[i].count != undefined) {
+                result[cash[i].name] = cash[i].count;
+            }
+        }
+        return result;
+    }
+    else return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+
 }
